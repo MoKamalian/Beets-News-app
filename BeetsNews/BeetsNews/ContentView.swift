e532fd1d58bdb2732d31c78570ad187f956ede53
Mo <am445669@dal.ca>
2021-08-29 16:48:36 -0300

    addition of list

--- BeetsNews/BeetsNews/ContentView.swift
+++ BeetsNews/BeetsNews/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // The navigationView is used to nagivate to different screens similar to before
+        NavigationView {
+            // Think of the List{} as a tableView. The implementation of the list below allows us to
+            // take the rows and compute them. So in this case we are taking every post and putting in the title.
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationTitle("Beets News")
+        }
     }
 }
 
@@ -19,3 +26,20 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// Below struct is what we can use to update our tabelview/list with the articles that we want.
+// This struct will conform to the Identifiable protocol which will allow us to update the list in an order based
+// on the id of the post.
+struct Posted: Identifiable {
+    let id: String
+    let title: String
+    
+}
+
+let posts = [
+    Posted(id: "1", title: "bruhh"),
+    Posted(id: "2", title: "beets"),
+    Posted(id: "3", title: "scoopity whoop")
+]
+
+
